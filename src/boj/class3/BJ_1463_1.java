d609c9e6d6f36a5b503c8109e967c1c0b81c0b76
dohun <shine_17@naver.com>
2024-10-05 18:42:53 +0900

    2024. 10. 05. 백준 1463

--- src/boj/class3/BJ_1463_1.java
+++ src/boj/class3/BJ_1463_1.java
@@ -11,6 +11,13 @@ public class BJ_1463_1 {
 
         try {
 
+            /*
+                DP 문제는 3가지 단계를 생각
+                1. 테이블 정의
+                2. 점화식 찾기
+                3. 초기값 정하기
+             */
+
             int n = Integer.parseInt(br.readLine());
 
             numArr = new Integer[n+1];
@@ -25,20 +32,20 @@ public class BJ_1463_1 {
 
         if(numArr[n] == null) {
             if(n % 6 == 0) {
-                numArr[n] = Math.min(Math.min(operate(n / 3), operate(n / 2)), operate(n - 1)) + 1;
+                numArr[n] = Math.min(Math.min(operate(n / 3), operate(n / 2)), operate(n - 1));
             }
             else if(n % 3 == 0) {
-                numArr[n] = Math.min(operate(n / 3), operate(n - 1)) + 1;
+                numArr[n] = Math.min(operate(n / 3), operate(n - 1));
             }
             else if(n % 2 == 0) {
-                numArr[n] = Math.min(operate(n / 2), operate(n - 1)) + 1;
+                numArr[n] = Math.min(operate(n / 2), operate(n - 1));
             }
             else {
-                numArr[n] = operate(n - 1) + 1;
+                numArr[n] = operate(n - 1);
             }
-        }
 
-//        System.out.println("[" + n + "] " + numArr[n]);
+            numArr[n]++;
+        }
 
         return numArr[n];
     }
