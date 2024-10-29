1631ab625b344d1d7ddad71c07946ab14fef4c16
dohun <shine_17@naver.com>
2024-10-29 16:43:01 +0900

    백준 문제풀이

--- src/boj/class3/BJ_18111_마인크래프트.java
+++ src/boj/class3/BJ_18111_마인크래프트.java
@@ -1,4 +1,4 @@
-package boj.class2;
+package boj.class3;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class BJ_18111 {
+public class BJ_18111_마인크래프트 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
