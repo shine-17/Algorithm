abd219a974a65fb2a01ce945e50d28475b6e15b2
dohun <shine_17@naver.com>
2024-12-06 13:32:26 +0900

    백준 문제풀이

--- src/boj/class3/BJ_7662_이중우선순위큐.java
+++ src/boj/class3/BJ_7662_이중우선순위큐.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class BJ_7662 {
+public class BJ_7662_이중우선순위큐 {
     public static void main(String[] args) {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
