1540d4f07ddb4f42aaf5a20d5abe0c05da128aea
dohun <shine_17@naver.com>
2025-06-01 23:28:57 +0900

    백준 문제풀이

---
+++ src/boj/class4/BJ_11444_피보나치수6.java
@@ -0,0 +1,20 @@
+package boj.class4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_11444_피보나치수6 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+
+            long n = Long.parseLong(br.readLine());
+
+
+
+
+
+        } catch (Exception ignored) {}
+    }
+}
