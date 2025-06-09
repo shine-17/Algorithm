7293bff1e59252434fd6816641c69291e1e80592
dohun <shine_17@naver.com>
2025-06-09 23:06:47 +0900

    백준 문제풀이

---
+++ src/boj/class4/BJ_11053_가장긴증가하는부분수열_1.java
@@ -0,0 +1,34 @@
+package boj.class4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BJ_11053_가장긴증가하는부분수열_1 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+
+            int n = Integer.parseInt(br.readLine());
+
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            int[] dp = new int[n+1];
+            dp[0] = 1;
+
+            for (int i=1; i<arr.length; i++) {
+                if (arr[i] > arr[i-1]) {
+                    dp[i] = dp[i-1]+1;
+
+                    for (int j=1; j<=i; j++) {
+                        dp[j] = Math.max(dp[j], arr[i]);
+                    }
+                }
+
+            }
+
+
+        } catch (Exception ignored) {}
+    }
+}
