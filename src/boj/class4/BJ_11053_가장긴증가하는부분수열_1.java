ea7630117f6942ada1cf8aa916cf5c8753eaaeb7
dohun <shine_17@naver.com>
2025-06-12 22:17:05 +0900

    백준 문제풀이

--- src/boj/class4/BJ_11053_가장긴증가하는부분수열_1.java
+++ src/boj/class4/BJ_11053_가장긴증가하는부분수열_1.java
@@ -11,23 +11,25 @@ public class BJ_11053_가장긴증가하는부분수열_1 {
         try {
 
             int n = Integer.parseInt(br.readLine());
-
             int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-            int[] dp = new int[n+1];
+            int[] dp = new int[n];
             dp[0] = 1;
 
             for (int i=1; i<arr.length; i++) {
                 if (arr[i] > arr[i-1]) {
                     dp[i] = dp[i-1]+1;
+                }
 
-                    for (int j=1; j<=i; j++) {
-                        dp[j] = Math.max(dp[j], arr[i]);
+                for (int j=i-1; j>=0; j--) {
+                    if (arr[i] > arr[j]) {
+                        dp[i] = Math.max(dp[i], dp[j]+1);
                     }
                 }
 
+                dp[i] = Math.max(dp[i], 1);
             }
 
+            System.out.println(Arrays.stream(dp).max().getAsInt());
 
         } catch (Exception ignored) {}
     }
