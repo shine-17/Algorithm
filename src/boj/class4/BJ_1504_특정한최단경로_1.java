f7a10bc2d1310a1318bc8015851ee5574ecae492
dohun <shine_17@naver.com>
2025-06-23 23:44:42 +0900

    백준 문제풀이

---
+++ src/boj/class4/BJ_1504_특정한최단경로_1.java
@@ -0,0 +1,16 @@
+package boj.class4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_1504_특정한최단경로_1 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+
+
+
+        } catch (Exception ignored) {}
+    }
+}
