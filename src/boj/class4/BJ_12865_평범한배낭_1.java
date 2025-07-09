16fdd152167b4b3ccb7ba91c7d8e19ab9b5d81dc
dohun <shine_17@naver.com>
2025-07-09 23:10:36 +0900

    백준 문제풀이

---
+++ src/boj/class4/BJ_12865_평범한배낭_1.java
@@ -0,0 +1,18 @@
+package boj.class4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_12865_평범한배낭_1 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        } catch (Exception ignored) {}
+    }
+}
